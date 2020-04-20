881bd8bd0dcb815f34031faf1839932ca60aed84
Alex <ungurean.alexandru.florin@gmail.com>
2020-04-20 13:28:40 +0300

    Add register

--- auth-service/controllers/authController.go
+++ auth-service/controllers/authController.go
@@ -52,7 +52,9 @@ func Register(responseWriter http.ResponseWriter, request *http.Request) {
     	connectToBD();
     }
 
-	rows, error := db.Query(`select email, password from users where email = '$1';`, user.email);
+    fmt.Println(user.email);
+
+	rows, error := db.Query(`select email, password from users where email = $1;`, user.email);
 	check(error);
 
 	var result string = "";
