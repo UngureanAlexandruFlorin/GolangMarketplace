0f07598169af74d79835a1f304c9d056982b5a04
Alex <ungurean.alexandru.florin@gmail.com>
2020-05-01 17:23:45 +0300

    Integrate ui-service

--- ui-service/src/Auth.js
+++ ui-service/src/Auth.js
@@ -20,11 +20,10 @@ class Auth extends React.Component {
         if (this.state.email.length < 2 || this.state.password < 2) {
             alert('Email or password too short!');
         } else {
-            fetch('http://localhost:8081/login', {
+            fetch('http://ec2-54-219-132-254.us-west-1.compute.amazonaws.com:8081/login', {
                 method: 'post',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
                     email: this.state.email,
