32404cfe52744d736de4cd5076077d0226f4e79c
Alex <ungurean.alexandru.florin@gmail.com>
2020-05-06 21:48:55 +0300

    Add CORS

--- auth-service/controllers/authController.go
+++ auth-service/controllers/authController.go
@@ -105,6 +105,7 @@ func connectToBD() {
 func enableCors(w *http.ResponseWriter) {
     (*w).Header().Set("Access-Control-Allow-Origin", "*");
     (*w).Header().Set("Content-Type", "application/json");
-    (*w).Header().Set("Access-Control-Allow-Headers", "Content-Type");
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
     (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    (*w).Header().Set("Access-Control-Allow-Credentials", "true");
 }
--- product-service/middlewares/authMiddleware.go
+++ product-service/middlewares/authMiddleware.go
@@ -83,6 +83,7 @@ func decodeJWT(responseWriter http.ResponseWriter, request *http.Request) bool {
 func enableCors(w *http.ResponseWriter) {
     (*w).Header().Set("Access-Control-Allow-Origin", "*");
     (*w).Header().Set("Content-Type", "application/json");
-    (*w).Header().Set("Access-Control-Allow-Headers", "Content-Type");
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
     (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    (*w).Header().Set("Access-Control-Allow-Credentials", "true");
 }
