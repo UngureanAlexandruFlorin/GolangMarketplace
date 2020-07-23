3e1a5d56e69df6299fc74907e01c9832bcda8b50
Alexandru Ungurean <alexandru.ungurean@airgapnetworks.com>
2020-07-23 12:43:10 +0300

    Change postgres IP address

--- auth-service/main.go
+++ auth-service/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "local.com/golangMarketplace/authService/controllers"
+	"fmt"
+	"net/http"
+
+	"local.com/golangMarketplace/authService/controllers"
 )
 
 func main() {
-    fmt.Printf("Auth service started on port 8081!\n");
-    
+	fmt.Printf("Auth service started on port 8081!\n")
+
 	http.HandleFunc("/login", controllers.Login)
 	http.HandleFunc("/register", controllers.Register)
 	http.ListenAndServe(":8081", nil)
