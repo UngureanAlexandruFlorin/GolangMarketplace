3e043a28e923d66e8b32d9dfbcebadd9cce4ce1c
Alexandru Ungurean <alexandru.ungurean@airgapnetworks.com>
2020-07-27 16:39:01 +0300

    Don't let UI reset at refresh

--- product-service/models/product.go
+++ product-service/models/product.go
@@ -3,36 +3,36 @@ package models
 import "github.com/dgrijalva/jwt-go"
 
 type AlterProduct struct {
-	JwtEmail string;
-	Id string;
-	Email string;
-	Name string;
-	Description string;
-	Price int;
+	JwtEmail    string
+	Id          string
+	Email       string
+	Name        string
+	Description string
+	Price       int
 }
 
 type Product struct {
-	Email string;
-	Name string;
-	Description string;
-	Price int;
+	Email       string
+	Name        string
+	Description string
+	Price       int
 }
 
 type ObjectID struct {
-	JwtEmail string;
-	Id string;
+	JwtEmail string
+	Id       string
 }
 
 type SellerEmail struct {
-	Email string;
+	Email string
 }
 
 type JwtClaims struct {
-	Email string;
-	jwt.StandardClaims;
+	Email string
+	jwt.StandardClaims
 }
 
-type AuthJwtData struct{
-	AuthEmail string;
-	Body interface{};
+type AuthJwtData struct {
+	AuthEmail string
+	Body      interface{}
 }
